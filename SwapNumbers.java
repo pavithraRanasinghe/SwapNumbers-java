1c402ad9743444cbc169af19ba9efc350bc4aea3
PavithraRanasinghe <paviranasinghe20@gamil.com>
2019-02-23 10:06:38 +0530

    Class updater

--- SwapNumbers.java
+++ SwapNumbers.java
@@ -1,1 +1,14 @@
-
+class SwapNumbers{
+	public static void main(String args[]){
+		int x=10;
+		int y=20;
+		System.out.println("x : "+x);
+		System.out.println("y : "+y);
+		int z;
+		z=x;
+		x=y;
+		y=z;
+		System.out.println("x : "+x);
+		System.out.println("y : "+y);
+	}
+}
